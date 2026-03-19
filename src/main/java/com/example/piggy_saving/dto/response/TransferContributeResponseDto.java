45484815c9eb4ab0c345ca54aa8014c845bfb454
Kimsan <yoeurnkimsan@gmail.com>
2026-03-20 02:19:14 +0700

    Modify complete snake_case Contribute

--- src/main/java/com/example/piggy_saving/dto/response/TransferContributeResponseDto.java
+++ src/main/java/com/example/piggy_saving/dto/response/TransferContributeResponseDto.java
@@ -3,6 +3,7 @@ package com.example.piggy_saving.dto.response;
 import com.example.piggy_saving.models.enums.GoalStatus;
 import com.example.piggy_saving.models.enums.TransactionType;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,15 +19,29 @@ import java.util.UUID;
 @AllArgsConstructor
 public class TransferContributeResponseDto {
 
+    @JsonProperty("transaction_id")
     private UUID transactionId;       // e.g., "tx-003"
+    @JsonProperty("sender_account_id")
     private UUID senderAccountId;     // clearer than fromAccountId
+    @JsonProperty("sender_account_number")
+    private String senderAccountNumber;
+    @JsonProperty("recipient_account_id")
     private UUID recipientAccountId;  // clearer than toAccountId
+    @JsonProperty("recipient_account_number")
+    private String recipientAccountNumber;
+    @JsonProperty("amount")
     private BigDecimal amount;          // contribution amount
+    @JsonProperty("transaction_type")
     private TransactionType transactionType;     // e.g., "contribution"
+    @JsonProperty("goal_name")
     private String goalName;            // e.g., "Birthday Fund"
+    @JsonProperty("goal_owner")
     private String goalOwner;           // e.g., "Jane Smith"
+    @JsonProperty("description")
     private String description;         // contribution description
+    @JsonProperty("current_main_balance")
     private BigDecimal newMainBalance;  // updated main account balance
+    @JsonProperty("status")
     private GoalStatus status;    // easier boolean naming
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
